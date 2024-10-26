311f7eec4dd8018a64941db1c5034b8956d4a713
ImRyzo <aqilyoga00@gmial.com>
2024-10-27 00:04:05 +0700

    Done use model AI training to the backend

---
+++ Web/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("pos_tagger_model.pkl", "rb"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/spok", methods=["GET", "POST"])
+def spok():
+    if request.method == "POST":
+        sentence = request.form["sentence"]
+        tagged_sentence = tag_sentence(model, sentence)
+        return render_template("spok.html", tagged_sentence=tagged_sentence)
+    return render_template("spok.html")
+
+def tag_sentence(tagger, sentence):
+    tokens = sentence.split()
+    return tagger.tag(tokens)
+
+if __name__ == "__main__":
+    app.run(debug=True)
